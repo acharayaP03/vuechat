d09e48a9e47b1ac56da865a50b562713478bebc1
Prabhakar Acharya <acharyaP03@gmail.com>
2020-11-15 19:40:01 +1100

    second commit

--- src/firebase/init.js
+++ src/firebase/init.js
@@ -4,7 +4,14 @@ import firestore from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 var firebaseConfig = {
-
+  apiKey: process.env.VUE_APP_FIRESTORE_API,
+  authDomain: process.env.VUE_APP_AUTHDOMAIN,
+  databaseURL: process.env.VUE_APP_DATABASE_URL,
+  projectId: process.env.VUE_APP_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_SOTRAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_MESSAGING_SENDER_ID,
+  appId: process.env.VUE_APP_ID,
+  measurementId: process.env.VUE_MESUREMENT_ID,
 };
 // Initialize Firebase
 const firebaseApp = firebase.initializeApp(firebaseConfig);
