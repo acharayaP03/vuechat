5e60f3b5858d0c46f0449f2fe99b199f93fba66f
Prabhakar Acharya <acharyaP03@gmail.com>
2020-11-16 22:33:17 +1100

    Compones, routes and routes guard

---
+++ src/views/Chat.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="chat container">
+    <h2>This is Chat page</h2>
+    <p class="teal-text" v-if="this.name">{{ this.name }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Chat",
+  props: ["name"],
+  data() {
+    return {};
+  },
+};
+</script>
+<style lang="scss" scoped></style>
