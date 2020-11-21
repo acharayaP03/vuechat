d5dbf92a5722ecb106d5d1526e8b581867c2a702
Prabhakar Acharya <acharyaP03@gmail.com>
2020-11-21 16:11:59 +1100

    Created new message component

--- src/views/Chat.vue
+++ src/views/Chat.vue
@@ -1,17 +1,48 @@
 <template>
   <div class="chat container">
-    <h2>This is Chat page</h2>
-    <p class="teal-text" v-if="this.name">{{ this.name }}</p>
+    <h2>Ninja Chat</h2>
+    <div class="card">
+      <div class="card-content">
+        <ul class="messages">
+          <li>
+            <span class="teal-text">Name</span>
+            <span class="grey-text text-darken-3">message</span>
+            <span class="grey-text time">time</span>
+          </li>
+        </ul>
+      </div>
+      <div class="card-action">
+        <NewMessage  :name="name"/>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import NewMessage from "@/components/NewMessage.vue";
 export default {
   name: "Chat",
   props: ["name"],
+  components:{
+    NewMessage
+  },
   data() {
     return {};
   },
 };
 </script>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.chat h2 {
+  font-size: 2.6em;
+  margin-bottom: 40px;
+}
+
+.chat span {
+  font-size: 1.4em;
+}
+
+.chat .time {
+  display: block;
+  font-size: 1.2em;
+}
+</style>
